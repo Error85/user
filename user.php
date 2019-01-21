ce4655a09e7f20c18045c70e0bd19969255342e0
Who Am I <46872860+Error85@users.noreply.github.com>
2019-01-21 16:47:59 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,10 @@
+<?php
+echo $red."[!] WARNING !!!".$t;
+echo $kuning."[-] Ini adalah program ilegal".$t;
+echo $kuning."[-] Resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
+
+$vip1= false;
+$we_stat = true;
+$msgwe = "[•] update vip versi 2.0 ";
+$msgveeu = "[!] enjoy your life";
+
