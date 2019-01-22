3d217552b4f30f833d3bdfa864317e7259f40b90
Error85 <46872860+Error85@users.noreply.github.com>
2019-01-22 18:19:57 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,14 @@
+<?php
+echo $red."[!] WARNING !!!".$t;
+echo $kuning."[-] Program Ilegal".$t;
+echo $kuning."[-] Resiko Sepenuhnya Di Tanggung Pengguna".$t.$ijo.$t;
+
+$we_stat = true;
+$msgwe = "[•] Update VIP Versi 2.0 ";
+$msgveeu = "[!] Enjoy Your Life...";
+
+$data= [
+
+"W96630",
+
+];
