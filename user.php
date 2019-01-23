acd17876190b6beaa4ea3d151159e53dd8b9c01e
Lupa Ingatan <46872860+Error85@users.noreply.github.com>
2019-01-23 17:15:19 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,24 @@
+<?php
+$baca_plus=$red."
+──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$kuning █▀▄ █── █─█ ▄▀▀────$red
+──── █▀█ █▀█ █─ █▀█────$kuning █─█ █─▄ █─█ ─▀▄────$red
+──── ▀▀─ ▀─▀ ─▀ ▀─▀────$kuning █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
+".$t;
+$msg_baca= "[+] Enjoy...";
+$stat_baca=true;
+ 
+$mc=$br."
+──── █▄─▄█ ▄▀▄ █▄─█ █▀▀ ▀▄─▄▀──── ▄▀ █─█ █▀▄ █▀▀────
+──── █─█─█ █─█ █─▀█ █▀▀ ──█────── █─ █─█ █▀█ █▀▀────
+──── ▀───▀ ─▀─ ▀──▀ ▀▀▀ ──▀────── ─▀ ─▀─ ▀▀─ ▀▀▀────
+".$t;
+$msg_mc=$ijo."[+] Enjoy...";
+$stat_mc=true;
+
+echo $red."[+] WARNING !!!".$t;
+echo $kuning."[+] Program ANU".$t;
+echo $kuning."[+] Resiko Sepenuhnya Ditanggung ANU..".$t.$ijo.$t;
+$msgwe = "[•] Update VIP v2.0 ";
+$we_stat = true;
+$vip1= false;
+?>
